64bf7ca53fd30072687e5b7bf42780388eb2640b
Alexander Anikin <devarkweb@gmail.com>
2024-06-11 21:48:12 +0300

    added config and update tests

--- index.js
+++ index.js
@@ -1,18 +1,58 @@
-function errity(cb, onError) {
+const syncSleep = (ms) => {
+  const initDate = Date.now();
+  while (Date.now() < initDate + ms) {}
+};
+
+const asyncSleep = (ms) => new Promise((res) => setTimeout(res, ms));
+
+function errity(cb, secondArg) {
+  // Второй аргумент может быть как функцией onError, так и конфигом
+  const isFunction = typeof secondArg === "function";
+  const isConfig = typeof secondArg === "object";
   const isAsync = cb.constructor.name == "AsyncFunction";
 
+  const config = isConfig ? secondArg : {};
+  const onErrorFunction = isFunction ? secondArg : undefined;
+
+  const {
+    onError = onErrorFunction,
+    onRetryError,
+    retryCount = 1,
+    retryDelay = 0,
+  } = config;
+
   const asyncApply = async (fn, ctx, args) => {
-    try {
-      return await fn.apply(ctx, args);
-    } catch (err) {
-      onError ? onError?.(err) : this?.defaultErrorCb?.(err);
+    for (let i = 0; i < retryCount; i++) {
+      if (retryDelay > 0 && i > 0) {
+        await asyncSleep(retryDelay);
+      }
+      try {
+        return await fn.apply(ctx, args);
+      } catch (error) {
+        if (i === retryCount - 1) {
+          onError ? onError?.(error) : this?.defaultErrorCb?.(error);
+        } else {
+          onRetryError?.(error);
+        }
+      }
     }
   };
+
   const syncApply = (fn, ctx, args) => {
-    try {
-      return fn.apply(ctx, args);
-    } catch (err) {
-      onError ? onError?.(err) : this?.defaultErrorCb?.(err);
+    for (let i = 0; i < retryCount; i++) {
+      if (retryDelay > 0 && i > 0) {
+        syncSleep(retryDelay);
+      }
+
+      try {
+        return fn.apply(ctx, args);
+      } catch (error) {
+        if (i === retryCount - 1) {
+          onError ? onError?.(error) : this?.defaultErrorCb?.(error);
+        } else {
+          onRetryError?.(error);
+        }
+      }
     }
   };
 
